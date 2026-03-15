8ee86cda98862ce075472c2a45dc5f8a5114cb47
Jjjkoddiz <hvsdicv@mail.ru>
2026-03-15 11:25:08 +0300

    3 module tasks

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,7 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filteredUsers = users.filter((user) => user.age <= age);
+
+  const lines = filteredUsers.map((user) => `${user.name}, ${user.balance}`);
+
+  return lines.join("\n");
 }
