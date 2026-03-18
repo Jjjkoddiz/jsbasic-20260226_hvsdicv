e6bd5c1d0bc3ecee13997a54eda3c2e66def1861
Jjjkoddiz <hvsdicv@mail.ru>
2026-03-18 17:48:02 +0300

    4 module tasks

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+
+    const cell = row.cells[i];
+
+    if (cell) {
+      cell.style.backgroundColor = "red";
+    }
+  }
 }
